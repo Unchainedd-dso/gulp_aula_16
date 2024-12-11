c166596feef6559f3b2db268aefdf8e77be59526
paulo <paulo02junior@hotmail.com>
2024-12-11 14:00:49 -0300

    commit inicial

---
+++ source/scripts/aritmetica.js
@@ -0,0 +1,15 @@
+function somar(numero1, numero2){
+    return numero1 + numero2;
+}
+
+function subtrair(numero1, numero2){
+    return numero1 - numero2;
+}
+
+function multiplicar(numero1, numero2){
+    return numero1 * numero2;
+}
+
+function dividir(dividendo, divisor){
+    return dividendo / divisor;
+}
